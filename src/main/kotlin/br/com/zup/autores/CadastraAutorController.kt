02a88f0a1b913243a1bdd97ed5b6d0348b4fee51
MarciaMartins <marciapmartins@gmail.com>
2021-08-09 16:54:31 -0300

    cadastro e recuperação de autor(es)

--- src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
@@ -1,7 +1,10 @@
 package br.com.zup.autores
 
+
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.validation.Validated
 import javax.validation.Valid
@@ -11,14 +14,19 @@ import javax.validation.Valid
 class CadastraAutorController(val autorRepository: AutorRepository) {
 
     @Post
-    fun cadastrar(@Body @Valid request: NovoAutorRequest){
+    fun cadastrar(@Body @Valid request: NovoAutorRequest): HttpResponse<DetalheAutorResponse>{
         println("Requisição => ${request}")
-
-        val autor = request.paraAutor()
-
-        
-        autorRepository.save(autor)
-
+        val autor: Autor = request.paraAutor()
+        var autorRespository: Autor = autorRepository.save(autor)
         println("Autor => ${autor.nome}")
+        val resposta = DetalheAutorResponse(autorRespository)
+        return HttpResponse.ok(resposta)
+    }
+
+    @Get
+    fun listarTodos(): HttpResponse<List<DetalheAutorResponse>> {
+        val autores =  autorRepository.findAll()
+        val resposta = autores.map { autor -> DetalheAutorResponse(autor) }
+        return HttpResponse.ok(resposta)
     }
 }
